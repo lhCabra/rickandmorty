76ee51d99f2b3d8542be01286caec0e3a0e64c71
unknown <lh.cabra@uniandes.edu.co>
2021-10-19 11:12:35 -0500

    cards

--- src/components/CardComponent.js
+++ src/components/CardComponent.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Card, Button } from 'react-bootstrap';
-export const CardComponent = () => {
+export const CardComponent = ({title, img, description}) => {
     return (
-<div className='container'>
+<div className='col-sm-2 container'>
       <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="https://source.unsplash.com/user/erondu/600x400" />
+      <Card.Img variant="top" src={img} />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>{title}</Card.Title>
         <Card.Text>
-          Some Custom text one can write here
+          {description}
         </Card.Text>
         <Button variant="primary">Go somewhere</Button>
       </Card.Body>
